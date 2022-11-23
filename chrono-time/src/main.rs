cbbc0d0b25c8041441218cb8039b8dafc6791bb4
daheige <zhuwei313@hotmail.com>
2022-11-23 23:23:18 +0800

    feat: chrono datetime

--- chrono-time/src/main.rs
+++ chrono-time/src/main.rs
@@ -1,4 +1,4 @@
-use chrono::{Datelike, Local, NaiveDateTime, Timelike};
+use chrono::{Datelike, Local, NaiveDate, NaiveDateTime, NaiveTime, Timelike};
 
 fn main() {
     let now = Local::now();
@@ -32,6 +32,13 @@ fn main() {
 
     println!("current time:{}", now.format("%Y-%m-%d %H:%M:%S"));
 
+    // 要解析不带时区的日期和时间，请使用 NaiveDate、NaiveTime，以及 NaiveDateTime
     let no_timezone = NaiveDateTime::parse_from_str("2015-09-05 23:56:04", "%Y-%m-%d %H:%M:%S");
     println!("{}", no_timezone.unwrap());
+
+    let date_only = NaiveDate::parse_from_str("2022-11-23", "%Y-%m-%d").unwrap();
+    println!("date:{}", date_only);
+
+    let time_only = NaiveTime::parse_from_str("23:56:04", "%H:%M:%S").unwrap();
+    println!("{}", time_only);
 }
