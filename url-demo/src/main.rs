6931c2d920bd78012b050422439fd7fd2c790484
daheige <zhuwei313@hotmail.com>
2022-12-01 22:58:02 +0800

    feat: url parse

--- url-demo/src/main.rs
+++ url-demo/src/main.rs
@@ -1,4 +1,5 @@
 use url::form_urlencoded::{byte_serialize, parse};
+use url::{Origin, ParseError, Position, Url};
 fn main() {
     /*
     使用 form_urlencoded::byte_serialize 将字符串编码为 application/x-www-form-urlencoded
@@ -17,4 +18,48 @@ fn main() {
     println!("decode str:{}", s);
 
     assert_eq!(s, url_str);
+}
+
+// 从基本 URL 创建新 URLs
+// url路径拼接
+fn build_url(base_url: &str, path: &str) -> Result<Url, ParseError> {
+    let base = Url::parse(base_url).expect("base_url invalid");
+    let joined = base.join(path)?;
+    Ok(joined)
+}
+
+#[test]
+fn url_parse() -> Result<(), ParseError> {
+    let s = "https://github.com/daheige?i=1&b=2&c=rust";
+    let parsed = Url::parse(s)?;
+    println!("url path: {}", parsed.path());
+    println!("domain:{:?}", parsed.domain());
+    println!("host:{:?}", parsed.host());
+    println!("port:{:?}", parsed.port().unwrap_or(443));
+    println!("https port:{:?}", parsed.port_or_known_default());
+    println!("scheme:{:?}", parsed.scheme());
+
+    // 也可以通过origin获取相关的信息
+    let origin = parsed.origin();
+    match origin {
+        Origin::Tuple(scheme, domain, port) => {
+            println!("scheme from origin:{}", scheme);
+            println!("domain from origin:{}", domain.to_string());
+            println!("port from origin:{}", port);
+        }
+        _ => {
+            return Err(ParseError::InvalidIpv4Address);
+        }
+    }
+
+    println!("origin:{:?}", parsed.origin());
+
+    // 从 URL 移除片段标识符和查询对
+    let cleaned: &str = &parsed[..Position::AfterPath];
+    println!("cleaned:{}", cleaned);
+
+    let user_url = build_url("https://github.com", "/daheige")?;
+    println!("user_url:{}", user_url);
+
+    Ok(())
 }
